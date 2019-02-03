b3a19ecb5035c2ea9793d208a2b198aaa181159e
Amanda Kay <amandakaywiggins@gmail.com>
2019-02-03 02:05:01 -0600

    work please

--- javascript/functionality.js
+++ javascript/functionality.js
@@ -2,7 +2,10 @@
 var config = {
   apiKey: "AIzaSyARWVD8RA9etmmLvsQYE9o3EqE_i36D1Yg",
   authDomain: "devdiary-43fd6.firebaseapp.com",
+  databaseURL: "https://devdiary-43fd6.firebaseio.com",
   projectId: "devdiary-43fd6",
+  storageBucket: "devdiary-43fd6.appspot.com",
+  messagingSenderId: "89880990619"
 };
 
 firebase.initializeApp(config);
