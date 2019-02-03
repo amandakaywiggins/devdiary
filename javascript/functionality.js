a127c297ca2f6a6eea786ded2818fdb8b921e614
Amanda Kay <amandakaywiggins@gmail.com>
2019-02-03 01:20:17 -0600

    updating db

--- javascript/functionality.js
+++ javascript/functionality.js
@@ -2,13 +2,8 @@
 var config = {
   apiKey: "AIzaSyARWVD8RA9etmmLvsQYE9o3EqE_i36D1Yg",
   authDomain: "devdiary-43fd6.firebaseapp.com",
-  databaseURL: "https://devdiary-43fd6.firebaseio.com",
   projectId: "devdiary-43fd6",
-  storageBucket: "devdiary-43fd6.appspot.com",
-  messagingSenderId: "89880990619"
 };
-
-firebase.initializeApp(config);
 
 var diaryData = firebase.firestore();
 
@@ -18,14 +13,11 @@ $("#addEntry").on("click", function() {
   var entryText = $("#text-input").val().trim();
   var entryTags = $("#tags-input").val().trim();
 
-  var newEntry = {
-
+  diaryData.collection(diary).add({
     title: entryTitle,
     text: netryText,
     tags: entryTags
-  };
-
-  diaryData.collection(diary).add({newEntry}).then(function(docRef) {
+  }).then(function(docRef) {
     console.log("Entry Added");
   }).catch(function(error) {
     console.log("Error");
